b681b8f08793d550f1b482021678d2712b43d46f
Chirag Sehgal <74814189+chiragsehgal29@users.noreply.github.com>
2024-06-12 23:59:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-// function combineDigits() {
-//     var leftDigits = document.getElementById("leftDigits").value;
-//     var rightDigits = document.getElementById("rightDigits").value;
-  
-//     if (leftDigits === "" || rightDigits === "") {
-//       alert("Please enter both left and right digits.");
-//       return;
-//     }
-  
-//     var output = document.getElementById("output");
-//     output.innerHTML = "";
-  
-//     for (var i = 0; i < leftDigits.length; i++) {
-//       for (var j = 0; j < rightDigits.length; j++) {
-//         var combination = parseInt(leftDigits[i] + rightDigits[j]);
-//         output.innerHTML += combination + ", ";
-//       }
-//     }
-//   }
-  
 function combineDigits() {
     var leftDigits = document.getElementById("leftDigits").value;
     var rightDigits = document.getElementById("rightDigits").value;
@@ -38,5 +18,14 @@ function combineDigits() {
     }
   }
 
-
-
+  function copyOutput() {
+    var outputText = document.getElementById("output").innerText.trim();
+    var textarea = document.createElement("textarea");
+    textarea.value = outputText;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textarea);
+    alert("Output copied to clipboard!");
+  }
+  
